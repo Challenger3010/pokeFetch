d9946ac284d1322a62dbfb91d52a51cef29a254b
Nick <hoernernick@web.de>
2026-03-10 14:01:56 +0100

    added REPL functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,5 +1,32 @@
+import * as readline from "readline";
+
 export function cleanInput(input: string): string[]{
-	const pokemons = input.trim().split(" ");
+	const pokemons = input
+						.trim()
+						.toLowerCase()
+						.split(" ")
+						.filter((word) => word !== "");
 	return pokemons;
 }
 
+export function startREPL(){
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	prompt: "Pokedex > ",
+});
+
+rl.prompt();
+
+rl.on("line", async (input) => {
+	const words = cleanInput(input);
+
+	if (words.length === 0){
+		rl.prompt();
+		return;
+	}
+
+	console.log(`Your command was: ${words[0]}`);
+	rl.prompt();
+});
+}
